79ee6187f17ce210376f696ead7cab8f62d59c51
Jonathan Rivera Chico <jonathan.rivera66@upr.edu>
2024-03-26 14:29:28 -0400

    Rendered sprites, deleted unnecessary parent folders

--- ASMaze/src/backgrounds.s
+++ ASMaze/src/backgrounds.s
@@ -40,7 +40,7 @@ load_sprites:
   LDA sprites,X
   STA $0200,X
   INX
-  CPX #$10
+  CPX #$c0   ; number of sprites that will be displayed
   BNE load_sprites
 
 	; write nametables
@@ -203,11 +203,11 @@ palettes:
 .byte $0f, $19, $09, $29
 
 sprites:
-;      y   tile attr  x
-.byte $00, $00, $00, $10
-.byte $00, $01, $00, $18   ; static front
-.byte $08, $10, $00, $10
-.byte $08, $11, $00, $18
+;      y   tile attr  x		; static front
+.byte $00, $00, $00, $00  ;left side of face
+.byte $00, $01, $00, $08  ;right side of face
+.byte $08, $10, $00, $00  ;left side of body
+.byte $08, $11, $00, $08  ;right side of face
 ; attr $03 = %0000 0011
 
 .byte $00, $02, $00, $10
@@ -215,15 +215,55 @@ sprites:
 .byte $08, $12, $00, $10
 .byte $08, $13, $00, $18
 
-.byte $00, $00, $00, $10
-.byte $00, $01, $00, $18   ; static front
-.byte $08, $10, $00, $10
-.byte $08, $11, $00, $18
+.byte $00, $04, $00, $20
+.byte $00, $05, $00, $28   ; moving front (right hand)
+.byte $08, $14, $00, $20
+.byte $08, $15, $00, $28
 
-.byte $00, $00, $00, $10
-.byte $00, $01, $00, $18   ; static front
-.byte $08, $10, $00, $10
-.byte $08, $11, $00, $18
+.byte $10, $06, $00, $00   ; back static
+.byte $10, $07, $00, $08   
+.byte $18, $16, $00, $00
+.byte $18, $17, $00, $08
+
+.byte $10, $08, $00, $10   ; moving back (left hand)
+.byte $10, $09, $00, $18   
+.byte $18, $18, $00, $10
+.byte $18, $19, $00, $18
+
+.byte $10, $0a, $00, $20   ; moving back (right hand)
+.byte $10, $0b, $00, $28   
+.byte $18, $1a, $00, $20
+.byte $18, $1b, $00, $28
+
+.byte $20, $0c, $00, $00   ; right static
+.byte $20, $0d, $00, $08   
+.byte $28, $1c, $00, $00
+.byte $28, $1d, $00, $08
+
+.byte $20, $0e, $00, $10   ; moving right (left hand)
+.byte $20, $0f, $00, $18   
+.byte $28, $1e, $00, $10
+.byte $28, $1f, $00, $18
+
+.byte $20, $20, $00, $20   ; moving right (right hand)
+.byte $20, $21, $00, $28   
+.byte $28, $30, $00, $20
+.byte $28, $31, $00, $28
+
+.byte $30, $22, $00, $00   ; left static
+.byte $30, $23, $00, $08   
+.byte $38, $32, $00, $00
+.byte $38, $33, $00, $08
+
+.byte $30, $24, $00, $10   ; moving left (left hand)
+.byte $30, $25, $00, $18   
+.byte $38, $34, $00, $10
+.byte $38, $35, $00, $18
+
+.byte $30, $26, $00, $20   ; moving left (right hand)
+.byte $30, $27, $00, $28   
+.byte $38, $36, $00, $20
+.byte $38, $37, $00, $28
 
 .segment "CHR"
 .incbin "graphics.chr"
